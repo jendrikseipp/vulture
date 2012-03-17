a4e3e658638153cb07209a1cbb84fd460733dbcd
Jendrik Seipp <jendrikseipp@gmail.com>
2012-03-17 18:19:09 +0100

    only add loaded attrs to used_attrs

--- wake.py
+++ wake.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# vulture - Find dead code.
+#
+# Copyright (C) 2012  Jendrik Seipp (jendrikseipp@web.de)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import ast
 from fnmatch import fnmatchcase
 import os
@@ -133,7 +152,8 @@ class Vulture(ast.NodeVisitor):
                 self.defined_funcs.append(self._get_item(node, 'function'))
 
     def visit_Attribute(self, node):
-        self.used_attrs.append(node.attr)
+        if isinstance(node.ctx, ast.Load):
+            self.used_attrs.append(node.attr)
 
     def visit_Name(self, node):
         if node.id != 'object':
