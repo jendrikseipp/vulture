0e70fbc2a2a92639db4dfa51204eec3920206819
Jendrik Seipp <jendrikseipp@gmail.com>
2014-05-09 10:22:15 +0200

    extend release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
 
+set -u
+set -e
+
+VERSION="$1"
+
 py.test-2.7
 py.test-3.3
+
+if [[ -n $(hg diff) ]]; then
+    echo "Error: repo has uncomitted changes"
+    exit 1
+fi
+
+# Bump version.
+sed -i -e "s/__version__ = '.*'/__version__ = '$VERSION'/" wake.py
+hg commit -m "Update version number to $VERSION for release."
+hg tag "v$VERSION"
+
 python setup.py register
 python setup.py sdist upload
