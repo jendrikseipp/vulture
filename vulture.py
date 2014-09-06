c60e4d9bd33c9570579823fcb914a3040d5d6307
Jendrik Seipp <jendrikseipp@gmail.com>
2014-09-06 16:05:53 +0200

    add shebang line, update copyright date

--- vulture.py
+++ vulture.py
@@ -1,8 +1,9 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # vulture - Find dead code.
 #
-# Copyright (C) 2012  Jendrik Seipp (jendrikseipp@web.de)
+# Copyright (C) 2012-2014  Jendrik Seipp (jendrikseipp@web.de)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
