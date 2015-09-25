f56c79fb21a16aaff71b3691ea03fee4c8e4cc31
Jendrik Seipp <jendrikseipp@gmail.com>
2015-09-25 16:02:29 +0200

    add 'setup.py test' command

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 #! /usr/bin/env python
 
+import sys
+
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 from vulture import __version__
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        sys.exit(pytest.main(self.test_args))
 
 
 setup(
@@ -31,4 +48,6 @@ setup(
     entry_points={
         'console_scripts': ['vulture = vulture:main'],
     },
+    tests_require=['pytest', 'pytest-cov'],
+    cmdclass={'test': PyTest},
 )
