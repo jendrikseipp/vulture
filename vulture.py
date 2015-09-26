915dd15c451db5ec04f7d3fc7f2d93714432e735
Jendrik Seipp <jendrikseipp@gmail.com>
2015-09-26 21:32:13 +0200

    update news, todo and copyright

--- vulture.py
+++ vulture.py
@@ -3,7 +3,7 @@
 #
 # vulture - Find dead code.
 #
-# Copyright (C) 2012-2014  Jendrik Seipp (jendrikseipp@web.de)
+# Copyright (C) 2012-2015  Jendrik Seipp (jendrikseipp@web.de)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
