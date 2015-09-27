7f65e74e75a68ff8e008c4a29a30c90218f743c3
Jendrik Seipp <jendrikseipp@gmail.com>
2015-09-27 18:52:33 +0200

    use clearer names

--- vulture.py
+++ vulture.py
@@ -66,7 +66,7 @@ class Vulture(ast.NodeVisitor):
         self.defined_vars = []
         self.used_vars = []
         self.tuple_assign_vars = []
-        self.asname_to_name = {}
+        self.import_aliases = {}
 
         self.file = ''
         self.code = None
@@ -187,11 +187,8 @@ class Vulture(ast.NodeVisitor):
 
     def visit_Name(self, node):
         if node.id != 'object':
+            self.log('used_funcs <-', node.id)
             self.used_funcs.append(node.id)
-            if node.id in self.asname_to_name:
-                orig_name = self.asname_to_name[node.id]
-                self.log('used_funcs <- %s asname %s' % (orig_name, node.id))
-                self.used_funcs.append(orig_name)
 
             if isinstance(node.ctx, ast.Load):
                 self.log('used_vars <-', node.id)
@@ -204,18 +201,19 @@ class Vulture(ast.NodeVisitor):
                     self.defined_vars.append(item)
 
     def visit_Import(self, node):
-        self._add_asnames(node)
+        self._add_aliases(node)
 
     def visit_ImportFrom(self, node):
-        self._add_asnames(node)
+        self._add_aliases(node)
 
-    def _add_asnames(self, node):
+    def _add_aliases(self, node):
         assert isinstance(node, (ast.Import, ast.ImportFrom))
-        for name_alias in node.names:
-            asname = name_alias.asname
-            if asname is not None:
-                self.log('asname alias <- %s -> %s' % (asname, name_alias.name))
-                self.asname_to_name[asname] = name_alias.name
+        for name_and_alias in node.names:
+            alias = name_and_alias.asname
+            if alias is not None:
+                name = name_and_alias.name
+                self.log('import_aliases <- %s : %s' % (name, alias))
+                self.import_aliases[name] = alias
 
     def _find_tuple_assigns(self, node):
         # Find all tuple assignments. Those have the form
