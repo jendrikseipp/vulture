02b3cd9ce56b13429ef7160b613e8473b7ec69a0
Rahul Jha <rahul722j@gmail.com>
2017-03-29 14:53:30 +0530

    Add more test cases for test_whitelist_python
    Add a test which would check wether or not there is an error raised
    when example_test file did not give an error despite of having some
    dead code (This will generally happen because of wrong configuration
    of tests, eg., when I mistakenly ran the test cases with python
    executable rather than vulture, example file obviously ran fine. :) )

--- tests/test_whitelist_python.py
+++ tests/test_whitelist_python.py
@@ -1,13 +1,12 @@
 import os.path
-from subprocess import call, Popen, PIPE
 import sys
+import subprocess
 
-DIR = os.path.dirname(os.path.abspath(__file__))
-REPO = os.path.dirname(DIR)
+from .test_script import call_vulture, REPO
 
 
 def test_whitelist_python():
-    assert call(
+    assert subprocess.call(
         [sys.executable, 'whitelist_python.py'], cwd=REPO) == 0
 
 
@@ -16,11 +15,6 @@ def test_whitelist_python_function():
     Run vulture over an example file (tests/example.py) containing
     keywords for testing whitelist_python and check output.
     """
-    example_file = 'tests/example.txt'
-    p = Popen([sys.executable, example_file, 'whitelist_python.py'],
-              stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate("")
-    if err.decode("utf-8").find("could not be found.") != -1:
-        raise IOError("%s file wasn't found" % example_file)
-    assert err == b''
-    assert output == b''
+    example_file = os.path.join(REPO, 'tests/example_whitelist_python.txt')
+    assert call_vulture([example_file, 'whitelist_python.py']) == 0
+    assert call_vulture([example_file]) == 1
