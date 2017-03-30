605ba5bc00114bc445b90a262a2b865848e90170
Rahul Jha <rahul722j@gmail.com>
2017-03-31 04:48:20 +0530

    Rename tests and alphabetical imports

--- tests/test_whitelist_python.py
+++ tests/test_whitelist_python.py
@@ -1,20 +1,20 @@
 import os.path
-import sys
 import subprocess
+import sys
 
 from .test_script import call_vulture, REPO
 
 
-def test_whitelist_python():
+def test_whitelist_python_with_python():
     assert subprocess.call(
         [sys.executable, 'whitelist_python.py'], cwd=REPO) == 0
 
 
-def test_whitelist_python_function():
+def test_whitelist_python_with_vulture():
     """
     Run vulture over an example file (tests/example.py) containing
     keywords for testing whitelist_python and check output.
     """
-    example_file = os.path.join(REPO, 'tests/example_whitelist_python.txt')
-    assert call_vulture([example_file, 'whitelist_python.py']) == 0
-    assert call_vulture([example_file]) == 1
+    whitelist_file = 'whitelist_python.py'
+    assert call_vulture([whitelist_file, 'whitelist_python.py']) == 0
+    assert call_vulture([whitelist_file]) == 0
