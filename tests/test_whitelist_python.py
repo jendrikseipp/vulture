fee5a37e58dd1edd60e8bcfed74c5aa78b087d04
Rahul Jha <rahul722j@gmail.com>
2017-03-31 14:10:22 +0530

    test whitelist_python by calling vulture on it
    Previousyly, we were testing our whitelist file by calling vulture on
    an example file and supplying whitelist_python.py. Now, we would call
    vulture on whitelist_file for the same.

--- tests/test_whitelist_python.py
+++ tests/test_whitelist_python.py
@@ -1,20 +1,15 @@
-import os.path
 import subprocess
 import sys
 
 from .test_script import call_vulture, REPO
 
+whitelist_file = 'whitelist_python.py'
+
 
 def test_whitelist_python_with_python():
     assert subprocess.call(
-        [sys.executable, 'whitelist_python.py'], cwd=REPO) == 0
+        [sys.executable, whitelist_file], cwd=REPO) == 0
 
 
 def test_whitelist_python_with_vulture():
-    """
-    Run vulture over an example file (tests/example.py) containing
-    keywords for testing whitelist_python and check output.
-    """
-    whitelist_file = 'whitelist_python.py'
-    assert call_vulture([whitelist_file, 'whitelist_python.py']) == 0
     assert call_vulture([whitelist_file]) == 0
--- whitelist_python.py
+++ whitelist_python.py
@@ -1,3 +1,4 @@
 import collections
 
+collections.defaultdict(list).default_factory = None
 collections.defaultdict(list).default_factory
