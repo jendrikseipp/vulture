ef926dee5a06e43acb83b046f58dd75261c30b6f
Jendrik Seipp <jendrikseipp@gmail.com>
2017-04-02 11:43:32 +0200

    move contents of whitelist.py to python_whitelist.py

--- TODO.rst
+++ TODO.rst
@@ -5,15 +5,23 @@ TODOs
 * Detect unreachable code à la `if False:` (try to evaluate condition).
 * Parse all variable names in new format strings (vars with special formatting, etc.)
   Use string.Formatter.parse for this.
-* Allow using vulture as a module. Possibly using pyflakes as an example library.
 * Once we drop Python 2.6 compatibility use argparse instead of optparse.
-* Use astroid if we ever want to support scopes.
+* Add whitelists directory. It should contain whitelist_python.py for now.
+  The file should contain code that traverses an AST and code that uses
+  these functions explicitly. Then whitelist.py becomes obsolete. It
+  should also contain the code and whitelist code for default_factory.
 
 
 Non-TODOs
 =========
 
 * Ignore hidden files and directories (might be unexpected, use --exclude instead).
-* Differentiate between functions and methods. For our purposes methods are 
-  functions with a "self" parameter and they are stored as attributes, not as 
-  variables (unclear if this has many benefits). 
+* Differentiate between functions and methods. For our purposes methods are
+  functions with a "self" parameter and they are stored as attributes, not as
+  variables (unclear if this has many benefits).
+
+
+Notes
+=====
+
+* Use astroid if we ever want to support scopes.
--- whitelist_python.py
+++ whitelist_python.py
@@ -1,4 +1,72 @@
+"""
+
+Vulture sometimes reports used code as unused. To avoid these
+false-positives, you can write a Python file that explicitly uses the
+code and pass it to vulture:
+
+vulture myscript.py mydir mywhitelist.py
+
+This file explicitly uses code from the Python standard library that is
+often incorrectly detected as unused.
+
+"""
+
+from ast import NodeVisitor
 import collections
 
+# NodeVisitor methods are called implicitly.
+class WhitelistNodeVisitor(NodeVisitor):
+    def visit_arg(self, node):
+        node
+        pass
+
+    def visit_alias(self, node):
+        pass
+
+    def visit_Assign(self, node):
+        pass
+
+    def visit_Attribute(self, node):
+        pass
+
+    def visit_ClassDef(self, node):
+        pass
+
+    def visit_comprehension(self, node):
+        pass
+
+    def visit_For(self, node):
+        pass
+
+    def visit_FunctionDef(self, node):
+        pass
+
+    def visit_Import(self, node):
+        pass
+
+    def visit_ImportFrom(self, node):
+        pass
+
+    def visit_Name(self, node):
+        pass
+
+    def visit_Str(self, node):
+        pass
+
+WhitelistNodeVisitor.visit_arg
+WhitelistNodeVisitor.visit_alias
+WhitelistNodeVisitor.visit_Assign
+WhitelistNodeVisitor.visit_Attribute
+WhitelistNodeVisitor.visit_ClassDef
+WhitelistNodeVisitor.visit_comprehension
+WhitelistNodeVisitor.visit_For
+WhitelistNodeVisitor.visit_FunctionDef
+WhitelistNodeVisitor.visit_Import
+WhitelistNodeVisitor.visit_ImportFrom
+WhitelistNodeVisitor.visit_Name
+WhitelistNodeVisitor.visit_Str
+
+
+# To free memory, the "default_factory" attribute can be set to None.
 collections.defaultdict(list).default_factory = None
 collections.defaultdict(list).default_factory
