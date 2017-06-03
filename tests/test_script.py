e5012c424fdba1a6aa0b5da8e9befdf2e98e0fc9
RJ722 <rahul.jha945@gmail.com>
2017-06-04 01:45:46 +0530

    Add tests for version

--- tests/test_script.py
+++ tests/test_script.py
@@ -2,6 +2,7 @@ import os.path
 import subprocess
 import sys
 
+from vulture import __version__
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 REPO = os.path.dirname(DIR)
@@ -11,6 +12,12 @@ WHITELIST = os.path.join(REPO, 'whitelists', 'stdlib.py')
 def call_vulture(args, **kwargs):
     return subprocess.call(
         [sys.executable, 'vulture.py'] + args, cwd=REPO, **kwargs)
+
+
+def get_output(args):
+    child = subprocess.Popen([sys.executable, "vulture.py"] + args,
+                             cwd=REPO, stdout=subprocess.PIPE)
+    return child.communicate()[0].decode("utf-8")
 
 
 def test_script_with_whitelist():
@@ -43,3 +50,8 @@ def test_whitelist_with_vulture():
 
 def test_pyc():
     assert call_vulture(['missing.pyc']) == 1
+
+
+def test_version():
+    assert call_vulture(['--version']) == 0
+    assert get_output(['--version']) == 'vulture.py {0}\n'.format(__version__)
