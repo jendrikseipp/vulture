3b7b38543967fdc554c7d0cd2cdcd9d9c64937d8
RJ722 <rahul722j@gmail.com>
2017-06-13 22:48:40 +0530

    Ship vulture as a package.

--- tests/test_script.py
+++ tests/test_script.py
@@ -6,30 +6,30 @@ from vulture import __version__
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 REPO = os.path.dirname(DIR)
-WHITELIST = os.path.join(REPO, 'whitelists', 'stdlib.py')
+WHITELIST = os.path.join(REPO, 'vulture', 'whitelists', 'stdlib.py')
 
 
 def call_vulture(args, **kwargs):
     return subprocess.call(
-        [sys.executable, 'vulture.py'] + args, cwd=REPO, **kwargs)
+        [sys.executable, 'vulture/core.py'] + args, cwd=REPO, **kwargs)
 
 
 def get_output(args):
-    child = subprocess.Popen([sys.executable, "vulture.py"] + args,
+    child = subprocess.Popen([sys.executable, "vulture/core.py"] + args,
                              cwd=REPO, stdout=subprocess.PIPE)
     return child.communicate()[0].decode("utf-8")
 
 
 def test_script_with_whitelist():
-    assert call_vulture(['vulture.py', WHITELIST]) == 0
+    assert call_vulture(['vulture/core.py', WHITELIST]) == 0
 
 
 def test_script_without_whitelist():
-    assert call_vulture(['vulture.py']) == 1
+    assert call_vulture(['vulture/core.py']) == 1
 
 
 def test_exclude():
-    assert call_vulture(['vulture.py', '--exclude', 'vulture.py']) == 0
+    assert call_vulture(['vulture/core.py', '--exclude', 'core.py']) == 0
 
 
 def test_missing_file():
---
+++ vulture/__init__.py
@@ -0,0 +1,4 @@
+from .core import __version__, Vulture
+
+assert __version__
+assert Vulture
