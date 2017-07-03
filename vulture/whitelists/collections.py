db86938a37313d47efef2a672d4b9c244a268200
Rahul Jha <rahul722j@gmail.com>
2017-07-04 02:01:54 +0530

    Choose whitelists based on imports (#63)
    Also, rename stdlib.py to whitelist_utils.py.

---
+++ vulture/whitelists/collections.py
@@ -0,0 +1,5 @@
+from whitelist_utils import Whitelist
+
+# To free memory, the "default_factory" attribute can be set to None.
+whitelist_defaultdict = Whitelist()
+whitelist_defaultdict.default_factory
