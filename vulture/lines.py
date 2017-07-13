afd4d3fb6ac6a7e38e22320d8556ba4c93946cd1
RJ722 <rahul722j@gmail.com>
2017-07-13 16:50:06 +0530

    Change _get_range to get_last_line

--- vulture/lines.py
+++ vulture/lines.py
@@ -1,8 +1,5 @@
 import ast
-from collections import namedtuple
 import sys
-
-ENTRY_POINTS = (ast.ClassDef, ast.FunctionDef)
 
 TRAVERSABLE_FIELDS = {
         ast.ClassDef: ('decorator_list', 'body', ),
@@ -25,26 +22,18 @@ else:
     })
 
 
-def _get_range(entry):
-    if isinstance(entry, ENTRY_POINTS):
-        last_body = entry.body[-1]
-        while isinstance(last_body, tuple(TRAVERSABLE_FIELDS.keys())):
-            fields = TRAVERSABLE_FIELDS[last_body.__class__]
-            for field in reversed(fields):
-                children = getattr(last_body, field)
-                if children:
-                    break
-            last_body = children[-1]  # The last child.
+def get_last_line_number(node):
+    last_body = node.body[-1]
+    while isinstance(last_body, tuple(TRAVERSABLE_FIELDS.keys())):
+        fields = TRAVERSABLE_FIELDS[last_body.__class__]
+        for field in reversed(fields):
+            children = getattr(last_body, field)
+            if children:
+                break
+        last_body = children[-1]
 
-        last_line = last_body.lineno
-        SourceRange = namedtuple("SourceRange",
-                                 ['name', 'start_line', 'last_line'])
-        source_range = SourceRange(entry.name, entry.lineno, last_line)
-        yield source_range
+    return last_body.lineno
 
 
-def count_lines(code):
-    span = 1
-    for source_range in _get_range(code):
-        span = (1 + source_range.last_line - source_range.start_line)
-    return span
+def count_lines(node):
+    return get_last_line_number(node) - node.lineno + 1
