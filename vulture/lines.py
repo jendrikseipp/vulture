f28ddd1ba69cb1f48b0d2c0babb4211e61236438
Rahul Jha <rahul722j@gmail.com>
2017-07-16 19:41:04 +0530

    Add count_lines() function (#70)
    Replace estimate_lines() function with faster and more accurate count_lines() function.

--- vulture/lines.py
+++ vulture/lines.py
@@ -1,49 +1,29 @@
-import ast
-from functools import wraps
-import sys
-
-TRAVERSABLE_FIELDS = {
-        ast.ClassDef: ('body', 'decorator_list'),
-        ast.ExceptHandler: ('body',),
-        ast.For: ('body', 'orelse'),
-        ast.FunctionDef: ('body', 'decorator_list'),
-        ast.If: ('body', 'orelse'),
-        ast.Module: ('body',),
-        ast.While: ('body', 'orelse'),
-        ast.With: ('body',),
-}
-if sys.version_info < (3, 0):
-    TRAVERSABLE_FIELDS.update({
-        ast.TryExcept: ('body', 'handlers', 'orelse'),
-        ast.TryFinally: ('body', 'finalbody'),
-    })
-else:
-    TRAVERSABLE_FIELDS.update({
-        ast.Try: ('body', 'handlers', 'orelse', 'finalbody')
-    })
-
-
-# Reinventing wheel here to keep vulture lightweight.
-def memoize(func):
-    _cache = {}
-
-    @wraps(func)
-    def wrapped(*args):
-        try:
-            return _cache[args]
-        except KeyError:
-            _cache[args] = result = func(*args)
-            return result
-    return wrapped
-
-
-@memoize
-def estimate_lines(node):
+def _get_last_child(node):
     """
-    Recursively count child AST nodes under `node`. It is an approximation
-    of the amount of code belonging to the node, which is useful for
-    sorting the list of unused code that a developer might want to remove.
+    Return the last direct child of `node`, or None if `node` has no children.
+
+    We test all fields that contain lists of nodes, choose the last field
+    that actually contains nodes and return the last node in the list.
     """
-    return 1 + sum(estimate_lines(child)
-                   for field in TRAVERSABLE_FIELDS.get(node.__class__, ())
-                   for child in getattr(node, field))
+    reverse_ordered_fields = ['finalbody', 'orelse', 'handlers', 'body']
+    for name in reverse_ordered_fields:
+        last_children = getattr(node, name, [])
+        if last_children:
+            return last_children[-1]
+    return None
+
+
+def get_last_line_number(node):
+    while True:
+        last_child = _get_last_child(node)
+        if last_child is None:
+            return node.lineno
+        node = last_child
+
+
+def count_lines(node):
+    """
+    Note: This function underestimates the size of code ending with multiline
+    statements and comments.
+    """
+    return get_last_line_number(node) - node.lineno + 1
