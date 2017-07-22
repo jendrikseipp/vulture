1d4e62f00d8da083efb4e74135f7e8a5842a913f
Jendrik Seipp <jendrikseipp@gmail.com>
2017-07-22 09:57:20 +0200

    change variable names

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
 # TODO: Convert used_vars and used_attrs to sets and use dedicated tests.
-def check(asserted, assertion):
-    names = set(getattr(item, 'name', item) for item in asserted)
-    assert names == set(assertion)
+def check(items_or_names, expected_names):
+    names = set(getattr(item, 'name', item) for item in items_or_names)
+    assert names == set(expected_names)
