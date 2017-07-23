a081ec66419457708405f6a58b36562cc419dcaf
Rahul Jha <rahul722j@gmail.com>
2017-07-23 15:17:39 +0530

    Add whitelist for unittest (#75)

---
+++ vulture/whitelists/unittest.py
@@ -0,0 +1,11 @@
+from whitelist_utils import Whitelist
+
+whitelist_test_case = Whitelist()
+whitelist_test_case.setUp
+whitelist_test_case.tearDown
+whitelist_test_case.setUpClass
+whitelist_test_case.tearDownClass
+whitelist_test_case.run
+whitelist_test_case.skipTest
+whitelist_test_case.subTest
+whitelist_test_case.debug
