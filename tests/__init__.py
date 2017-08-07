d4fa3a3fdf5a93120d7de2bd5c488610d670f244
RJ722 <rahul722j@gmail.com>
2017-08-08 00:00:59 +0530

    Add tests for min_confidence

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,29 @@
+import ast
 import pytest
+import sys
 from vulture import Vulture
+
+HAS_ASYNC = hasattr(ast, 'AsyncFunctionDef')
+PY3 = sys.version_info >= (3, )
+PY2 = sys.version_info < (3, )
+
+
+def skip_if_not_has_async(function):
+    if not HAS_ASYNC:
+        pytest.mark.skip(
+            function, reason="needs async support (added in Python 3.5)")
+
+
+def skip_if_python_3(function):
+    if PY3:
+        pytest.mark.skip(
+            function, reason="This test was meant for python 2")
+
+
+def skip_if_python_2(function):
+    if PY2:
+        pytest.mark.skip(
+            function, reason="needs ast.visit_args (added in python3)")
 
 
 @pytest.fixture
--- tests/test_confidence.py
+++ tests/test_confidence.py
@@ -1,45 +1,87 @@
-from . import v
-assert v
+from vulture import Vulture
+
+from . import skip_if_python_2
+from . import skip_if_python_3
 
 
-def test_confidence_import(v):
-    v.scan("""\
+def check_min_confidence(code, min_conf, linenos, confidences):
+    v = Vulture(verbose=True, min_confidence=min_conf)
+    v.scan(code)
+    for i, unused_item in enumerate(v.get_unused_code()):
+        assert unused_item.lineno == linenos[i]
+        assert unused_item.confidence == confidences[i]
+
+
+def test_confidence_import():
+    code = """\
 import foo
-a = 1
-""")
-    assert v.unused_imports[0].confidence == 90
+"""
+    check_min_confidence(code, 50, [1], [90])
 
 
-def test_confidence_unreachable(v):
-    v.scan("""\
+def test_confidence_unreachable():
+    code = """\
 def foo():
     return
     bar()
 
 foo()
-""")
-    assert v.unreachable_code[0].confidence == 100
+"""
+    check_min_confidence(code, 50, [3], [100])
+    check_min_confidence(code, 100, [3], [100])
 
 
-def test_confidence_others(v):
-    v.scan("""
-a = 1
+@skip_if_python_3
+def test_function_arg_py2():
+    code = """\
+def foo(a):
+    b = 3
 
-def foo():
-    pass
+foo(5)
+"""
+    check_min_confidence(code, 100, [], [])
+    check_min_confidence(code, 60, [1, 2], [60, 60])
+    check_min_confidence(code, 50, [1, 2], [60, 60])
 
 
+@skip_if_python_2
+def test_function_arg_py3():
+    code = """\
+def foo(a):
+    b = 3
+
+foo(5)
+"""
+    check_min_confidence(code, 100, [1], [100])
+    check_min_confidence(code, 60, [1, 2], [100, 60])
+
+
+def test_confidence_class():
+    code = """\
 class Foo:
+    pass
+"""
+    check_min_confidence(code, 50, [1], [60])
 
+
+def test_confidence_attr():
+    code = """\
+class Foo:
     self.b = 'something'
 
+Foo()
+"""
+    check_min_confidence(code, 50, [2], [60])
+
+
+def test_confidence_props():
+    code = """\
+class Foo:
     @property
     def some_prop():
         pass
 
-""")
-    assert v.unused_funcs[0].confidence == 60
-    assert v.unused_classes[0].confidence == 60
-    assert v.unused_vars[0].confidence == 60
-    assert v.unused_props[0].confidence == 60
-    assert v.unused_attrs[0].confidence == 60
+Foo()
+"""
+    check_min_confidence(code, 50, [2], [60])
+    check_min_confidence(code, 100, [], [])
