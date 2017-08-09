646f91a68dcab153676234d55bf0c453a83f9041
RJ722 <rahul722j@gmail.com>
2017-08-09 08:28:46 +0530

    Take testing arguments as kwargs

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 import ast
 import pytest
-import sys
+
 from vulture import Vulture
 
 
@@ -8,18 +8,6 @@ def skip_if_not_has_async(function):
     if not hasattr(ast, 'AsyncFunctionDef'):
         pytest.mark.skip(
             function, reason="needs async support (added in Python 3.5)")
-
-
-def skip_if_python_3(function):
-    if sys.version_info >= (3, 0):
-        pytest.mark.skip(
-            function, reason="This test is meant for Python 2")
-
-
-def skip_if_python_2(function):
-    if sys.version_info < (3, 0):
-        pytest.mark.skip(
-            function, reason="needs ast.visit_args (added in Python 3)")
 
 
 @pytest.fixture
--- tests/test_confidence.py
+++ tests/test_confidence.py
@@ -1,24 +1,24 @@
-from vulture import core
+import sys
 
-from . import skip_if_python_2
-from . import skip_if_python_3
+from vulture import core
 
 dc = core.DEFAULT_CONFIDENCE
 
 
-def check_min_confidence(code, min_conf, tests):
-    v = core.Vulture(verbose=True, min_confidence=min_conf)
+def check_min_confidence(code, min_confidence, expected_dict={}, **expected):
+    v = core.Vulture(verbose=True, min_confidence=min_confidence)
     v.scan(code)
-    for item, (lineno, confidence) in zip(v.get_unused_code(), tests):
-        assert item.lineno == lineno
-        assert item.confidence == confidence
+    detected = dict((item.name, item.confidence)
+                    for item in v.get_unused_code())
+    expected = expected_dict or expected
+    assert detected == expected
 
 
 def test_confidence_import():
     code = """\
 import foo
 """
-    check_min_confidence(code, 50, [(1, 90)])
+    check_min_confidence(code, 50, foo=90)
 
 
 def test_confidence_unreachable():
@@ -29,33 +29,23 @@ def foo():
 
 foo()
 """
-    check_min_confidence(code, 50, [(3, 100)])
-    check_min_confidence(code, 100, [(3, 100)])
+    check_min_confidence(code, 50, {'return': 100})
+    check_min_confidence(code, 100, {'return': 100})
 
 
-@skip_if_python_3
-def test_function_arg_py2():
+def test_function_arg():
     code = """\
 def foo(a):
     b = 3
 
 foo(5)
 """
-    check_min_confidence(code, 100, [])
-    check_min_confidence(code, dc, [(1, dc), (2, dc)])
-    check_min_confidence(code, 50, [(1, dc), (2, dc)])
-
-
-@skip_if_python_2
-def test_function_arg_py3():
-    code = """\
-def foo(a):
-    b = 3
-
-foo(5)
-"""
-    check_min_confidence(code, 100, [(1, 100)])
-    check_min_confidence(code, dc, [(1, 100), (2, dc)])
+    if sys.version_info < (3, 0):
+        check_min_confidence(code, 50, a=dc, b=dc)
+        check_min_confidence(code, dc, a=dc, b=dc)
+    if sys.version_info > (3, 0):
+        check_min_confidence(code, 50, a=100, b=dc)
+        check_min_confidence(code, dc, a=100, b=dc)
 
 
 def test_confidence_class():
@@ -63,17 +53,12 @@ def test_confidence_class():
 class Foo:
     pass
 """
-    check_min_confidence(code, 50, [(1, dc)])
+    check_min_confidence(code, 50, Foo=dc)
 
 
 def test_confidence_attr():
-    code = """\
-class Foo:
-    self.b = 'something'
-
-Foo()
-"""
-    check_min_confidence(code, 50, [(2, dc)])
+    code = "A.b = 'something'"
+    check_min_confidence(code, 50, b=dc)
 
 
 def test_confidence_props():
@@ -85,5 +70,5 @@ class Foo:
 
 Foo()
 """
-    check_min_confidence(code, 50, [(2, dc)])
-    check_min_confidence(code, 100, [])
+    check_min_confidence(code, 50, some_prop=dc)
+    check_min_confidence(code, 100)
