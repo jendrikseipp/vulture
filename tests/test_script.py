aa021241364a7454e5c1de7375c3752a80efff85
Jendrik Seipp <jendrikseipp@gmail.com>
2017-08-10 09:56:07 +0200

    cleanup tests

--- tests/test_script.py
+++ tests/test_script.py
@@ -3,8 +3,6 @@ import os.path
 import subprocess
 import sys
 
-from vulture import __version__
-
 DIR = os.path.dirname(os.path.abspath(__file__))
 REPO = os.path.dirname(DIR)
 WHITELISTS = glob.glob(os.path.join(REPO, 'vulture', 'whitelists', '*.py'))
@@ -18,38 +16,27 @@ def call_vulture(args, **kwargs):
         [sys.executable, '-m', core] + args, cwd=REPO, **kwargs)
 
 
-def get_output(args):
-    child = subprocess.Popen([sys.executable, "vulture/core.py"] + args,
-                             cwd=REPO, stdout=subprocess.PIPE)
-    return child.communicate()[0].decode("utf-8")
+def test_module_with_explicit_whitelists():
+    assert call_vulture(['vulture/'] + WHITELISTS) == 0
 
 
-def test_script_with_explicit_whitelist():
-    for whitelist in WHITELISTS:
-        assert call_vulture(['vulture/core.py', whitelist]) == 0
+def test_module_with_implicit_whitelists():
+    assert call_vulture(['vulture/']) == 0
 
 
-def test_script_with_implicit_whitelist():
-    assert call_vulture(['vulture/core.py']) == 0
-
-
-def test_script_without_whitelist():
-    assert call_vulture(['vulture/core.py', '--exclude', 'whitelists']) == 1
-
-
-def test_exclude():
-    assert call_vulture(['vulture/core.py', '--exclude', 'core.py']) == 0
+def test_module_without_whitelists():
+    assert call_vulture(['vulture/', '--exclude', 'whitelists']) == 1
 
 
 def test_missing_file():
     assert call_vulture(['missing.py']) == 1
 
 
-def test_dir():
-    assert call_vulture(['tests']) == 0
+def test_tests():
+    assert call_vulture(['tests/']) == 0
 
 
-def test_whitelist_with_python():
+def test_whitelists_with_python():
     for whitelist in WHITELISTS:
         assert subprocess.call([sys.executable, whitelist], cwd=REPO) == 0
 
@@ -58,14 +45,11 @@ def test_pyc():
     assert call_vulture(['missing.pyc']) == 1
 
 
-def test_version():
-    assert call_vulture(['--version']) == 0
-    assert get_output(['--version']).strip() == 'vulture ' + __version__
-
-
-def test_module():
-    assert call_vulture(['vulture/']) == 0
-
-
 def test_sort_by_size():
     assert call_vulture(['vulture/', '--sort-by-size']) == 0
+
+
+def test_min_confidence():
+    assert call_vulture([
+        'vulture/core.py', '--exclude', 'whitelists',
+        '--min-confidence', '100']) == 0
