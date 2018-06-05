4c0d0bbd25a7e234046da9d290d26969b4119414
RJ722 <rahul722j@gmail.com>
2018-06-05 13:35:10 +0530

    Test for Item.__repr__

---
+++ tests/test_item.py
@@ -0,0 +1,18 @@
+from . import v
+assert v  # Silence pyflakes
+
+
+def test_item_repr(v):
+    v.scan("""\
+import os
+
+
+message = "Foo Bar"
+
+
+class Foo:
+    def bar():
+        pass
+""")
+    for item in v.get_unused_code():
+        assert repr(item) == "'{}'".format(item.name)
