32cb89adbc77e040303aea8ec15aa724aa561aaf
RJ722 <rahul722j@gmail.com>
2018-07-24 18:07:39 +0530

    It is separated, not seperated.

--- vulture/core.py
+++ vulture/core.py
@@ -522,7 +522,7 @@ def _parse_args():
         ' *PATTERN*.')
     parser.add_argument(
             '--ignore-names', metavar='PATTERN', type=csv, default=None,
-            help='Comma-seperated list of names to ignore. Arguments may'
+            help='Comma-separated list of names to ignore. Arguments may'
             ' contain globbing characters (*, ?, [abc], [^abc]).')
     parser.add_argument(
             '--make-whitelist', action='store_true',
