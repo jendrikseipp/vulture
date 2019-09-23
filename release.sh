ab7f4c1c01316c7b12916f5b695488257764a53e
Jendrik Seipp <jendrikseipp@gmail.com>
2019-09-23 21:56:17 +0200

    Use twine to upload package to PyPI.

--- release.sh
+++ release.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 VERSION="$1"
 
+# Check dependencies.
+twine -h
+
 tox
 
 # Check that NEWS file is up-to-date.
@@ -27,8 +30,8 @@ sed -i -e "s/__version__ = '.*'/__version__ = '$VERSION'/" vulture/core.py
 git commit -am "Update version number to ${VERSION} for release."
 git tag -a "v$VERSION" -m "v$VERSION" HEAD
 
-python setup.py sdist upload
-python setup.py bdist_wheel upload
+python setup.py sdist bdist_wheel
+twine upload dist/vulture-${VERSION}.tar.gz dist/vulture-${VERSION}-py2.py3-none-any.whl
 
 git push
 git push --tags
