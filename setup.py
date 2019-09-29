9b4d3c2cb87114d1983ed50cc3ec0976964349c4
Jendrik Seipp <jendrikseipp@gmail.com>
2019-09-29 18:00:10 +0200

    List supported Python versions in setup.py file.

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setuptools.setup(
     entry_points={
         'console_scripts': ['vulture = vulture.core:main'],
     },
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     tests_require=['pytest', 'pytest-cov'],
     cmdclass={'test': PyTest},
     packages=setuptools.find_packages(exclude=['tests']),
