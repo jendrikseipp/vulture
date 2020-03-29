76da7ae3f413495ccfc1ac2b4349f4c1af0b9d38
RJ722 <rahul722j@gmail.com>
2020-03-30 02:26:39 +0530

    Add copyright header to all vulture/*.py files

--- vulture/core.py
+++ vulture/core.py
@@ -2,7 +2,7 @@
 #
 # vulture - Find dead code.
 #
-# Copyright (c) 2012-2019 Jendrik Seipp (jendrikseipp@gmail.com)
+# Copyright (c) 2012-2020 Jendrik Seipp (jendrikseipp@gmail.com)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
