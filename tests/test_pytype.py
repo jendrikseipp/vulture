1e1cdcea5abc94aee0b9c7791448a4c770394535
johndoknjas <jdoknjas@sfu.ca>
2024-10-02 05:59:04 -0700

    Satisfy black and ruff.

--- tests/test_pytype.py
+++ tests/test_pytype.py
@@ -1,9 +1,10 @@
 import subprocess
-from subprocess import PIPE
+
 
 def test_pytype():
     result = subprocess.run(
-        ['pytype', 'vulture/core.py', '--disable=attribute-error'],
-        stdout=PIPE, stderr=PIPE, universal_newlines=True
+        ["pytype", "vulture/core.py", "--disable=attribute-error"],
+        capture_output=True,
+        text=True,
     )
-    assert result.stdout.strip().endswith('Success: no errors found')
+    assert result.stdout.strip().endswith("Success: no errors found")
