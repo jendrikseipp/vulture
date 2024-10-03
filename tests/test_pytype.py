6bc297f480f28bc8da13dad8d25a74fb471ed200
johndoknjas <jdoknjas@sfu.ca>
2024-10-03 04:07:16 -0700

    Only run pytype for python < 3.13.

--- tests/test_pytype.py
+++ tests/test_pytype.py
@@ -1,6 +1,11 @@
 import subprocess
+import sys
+import pytest
 
 
+@pytest.mark.skipif(
+    sys.version_info >= (3, 13), reason="Need python < 3.13 for pytype"
+)
 def test_pytype():
     result = subprocess.run(
         ["pytype", "vulture/core.py", "--disable=attribute-error"],
