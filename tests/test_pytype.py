9415be75a98de75df9d47db2874ce7490e1e396e
johndoknjas <jdoknjas@sfu.ca>
2024-10-04 00:30:54 -0700

    Apply PR feedback (includes defining `self.noqa_lines` in the constructor and checking whether the pytype exit code is 0).

--- tests/test_pytype.py
+++ tests/test_pytype.py
@@ -1,15 +1,17 @@
 import subprocess
 import sys
+
 import pytest
 
 
 @pytest.mark.skipif(
-    sys.version_info >= (3, 13), reason="Need python < 3.13 for pytype"
+    sys.version_info >= (3, 13), reason="needs Python < 3.13 for pytype"
 )
 def test_pytype():
     result = subprocess.run(
-        ["pytype", "vulture/core.py", "--disable=attribute-error"],
+        ["pytype", "vulture/core.py"],
         capture_output=True,
         text=True,
+        check=True,
     )
     assert result.stdout.strip().endswith("Success: no errors found")
