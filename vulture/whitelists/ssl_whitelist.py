3c2bef412d5c6860a1b91dc784e590565a6d5e6d
Jesse Noordegraaf <150493071+tunnelsociety@users.noreply.github.com>
2025-06-04 15:51:56 -0400

    Add whitelist for ssl.SSLContext (#392)

---
+++ vulture/whitelists/ssl_whitelist.py
@@ -0,0 +1,15 @@
+import ssl
+
+ctx = ssl.SSLContext()
+ctx.check_hostname
+ctx.hostname_checks_common_name
+ctx.keylog_filename
+ctx.maximum_version
+ctx.minimum_version
+ctx.options
+ctx.post_handshake_auth
+ctx.sni_callback
+ctx.sslobject_class
+ctx.sslsocket_class
+ctx.verify_flags
+ctx.verify_mode
